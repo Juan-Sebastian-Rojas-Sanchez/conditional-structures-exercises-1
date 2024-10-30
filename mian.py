4a9d362aff85a6fe7d3972ee5aa89de8a13bf86d
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-30 12:58:58 -0500

    solucion ejercicio año bisiesto

--- mian.py
+++ mian.py
@@ -1,16 +1,32 @@
-#Escriba un programa que determine si el número entero ingresado por el usuario es par o no.
+#Cuando la Tierra completa una órbita alrededor del Sol, no han transcurrido exactamente 365 rotaciones sobre sí misma, sino un poco más. Más precisamente, la diferencia es de más o menos un cuarto de día.
+#
+#Para evitar que las estaciones se desfasen con el calendario, el calendario juliano introdujo la regla de introducir un día adicional en los años divisibles por 4 (llamados bisiestos), para tomar en consideración los cuatro cuartos de día acumulados.
+#
+#Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente 3/400 de día.
+#
+#Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo calendario, en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que fuera divisible por 400.
+#
+#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
+#
+#Ingrese un anno: 1988
+#1988 es bisiesto
+#Ingrese un anno: 2011
+#2011 no es bisiesto
+#Ingrese un anno: 1700
+#1700 no es bisiesto
+#Ingrese un anno: 1500
+#1500 es bisiesto
+##Ingrese un anno: 2400
+##2400 es bisiesto
+year = int(input("Enter a year: "))
 
-#`Ingrese un número: **4**
-#Su número es par`
+def es_bisiesto(year):
+    if year < 1582: 
+        return year % 4 == 0  
+    else:
+        return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-#`Ingrese un número: **3**
-#Su número es impar`
-
-
-
-nomber= int(input("write your number: "))
-
-if nomber %2 == 0:
-    print("Its number is even")
+if es_bisiesto(year):
+    print(f"{year} is leap year")
 else:
-    print("Its number is odd")
+    print(f"{year} it's not a leap year")
