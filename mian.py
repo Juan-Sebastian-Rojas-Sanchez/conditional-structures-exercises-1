973263f2b02c334b62267e0286bb87a0e7c381ec
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-31 10:38:28 -0500

    solucion ejercicio tennis

--- mian.py
+++ mian.py
@@ -1,39 +1,17 @@
-#Escriba un programa que entregue la edad del usuario a partir de su fecha de nacimiento:
+print ("Exercise 9 TENNIS SET")
 
-#Ingrese su fecha de nacimiento.
-#Dia: 14
-#Mes: 6
-#Anno: 1948
-#Usted tiene 62 annos
-#Por supuesto, el resultado entregado depende del día en que su programa será ejecutado.
+p1 = int(input("games won by A: "))
+p2 = int(input("games won by B: "))
 
-#Para obtener la fecha actual, puede hacerlo usando la función localtime que viene en el módulo time. Los valores se obtienen de la siguiente manera (suponiendo que hoy es 11 de marzo de 2011):
+#Conocer si es invalido
+if p1 > 7 or p2 > 7 or ( p1==7 and p2 < 5) or (p2==7 and p1 < 5) or abs(p1-p2) > 2:
+    print ("Invalid")
+#Conocer si aun no finaliza
+elif (p1 <= 6 and p2 <= 6 ) or (p1==6 and p2==6):
+    print ("It's not over yet")
+#Conocer si gana P1
+elif (p1 == 6 and p2 <= 4) or (p1 == 7 and p2 == 5) or (p1 == 7 and p2 == 6):
+    print ("Won A")
 
-#>>> from time import localtime
-#>>> t = localtime()
-#>>> t.tm_mday
-#11
-#>>> t.tm_mon
-#3
-#>>> t.tm_year
-#2011
-#El programa debe tener en cuen#ta si el cumpleaños ingresado ya pasó durante este año, o si todavía no ocu
-# rre
-# .
-from time import localtime
-
-t = localtime()
-day_current = t.tm_mday
-month_current = t.tm_mon
-year_current = t.tm_year
-
-day_birth = int(input("Enter your birth date.\nDay: "))
-month_birth = int(input("Month: "))
-year_birth = int(input("Year: "))
-
-age = year_current - year_birth
-
-if (month_birth > month_current) or (month_birth == month_current and day_birth > day_current):
-    age -= 1
-
-print(f"You are {age} years old.")
+elif (p2 == 6 and p1 <= 4) or (p2 == 7 and p1 == 5) or (p2 == 7 and p1 == 6):
+    print ("Won B")
