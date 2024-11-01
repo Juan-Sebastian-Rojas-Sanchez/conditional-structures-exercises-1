feef7bbbbb7895de4b7570ed5333114557c468d3
Juan-Sebastian-Rojas-Sanchez <[186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com](mailto:186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com)>
2024-11-01 11:08:46 -0500

    Potencias de dos

--- mian.py
+++ mian.py
@@ -1,6 +1,9 @@
-while True:
-    
-    numero= float(input("escribe un numero : "))
+#Potencias de dos
+# Get user input
+num = int(input("Enter a number: "))
 
-    if numero %2 == 0 and numero > 10:
-        break
+# Generate powers of 2
+powers_of_two = [2 ** i for i in range(num + 1)]
+
+# Print the results
+print(" ".join(map(str, powers_of_two)))
