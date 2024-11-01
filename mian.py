339909da454da11f74fe1fd68f160aa3f8b92030
Juan-Sebastian-Rojas-Sanchez <[186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com](mailto:186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com)>
2024-11-01 11:07:44 -0500

    ejercicio multiplos

--- mian.py
+++ mian.py
@@ -1,20 +1,6 @@
-print ("Exercise 11 Body mass index")
+while True:
+    
+    numero= float(input("escribe un numero : "))
 
-stature  = float(input("Insert stature: "))
-Weight   = float(input("Insert weight: "))
-age      = float(input("Insert age: "))
-
-imc  = Weight / (stature**2)
-
-if age < 45:
-    if imc < 22.0:
-        risk = "low"
-    else:
-        risk = "medium"
-else:
-    if imc < 22.0:
-        risk = "medium"
-    else:
-        risk = "high"
-
-print(f"Your risk of coronary heart disease is {risk}.")
+    if numero %2 == 0 and numero > 10:
+        break
